28d46d499b0663078163074fcef26185ea80647e
Jesus Barboza <jbarz20@gmail.com>
2022-10-13 20:15:57 -0500

    ADD: create controller of categories.

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,50 @@
+class CategoryController < ApplicationController
+  def index
+    @categories = Category.all
+
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def destroy
+    @cateogry = Cateogry.find(params[:id])
+    @cateogry.destroy
+
+    redirect_to categories_path, status: :see_other
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description, :cover)
+  end
+end

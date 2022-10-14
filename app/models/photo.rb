93fb7510e0602afb335425434b891bd0382d548c
Jesus Barboza <jbarz20@gmail.com>
2022-10-13 22:23:07 -0500

    CHORE: update the count photo cache.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, counter_cache: true
   # has_one_attached :cover
   has_many :comments, as: :commentable, dependent: :destroy
 
-  validates :name, presence: true, length: { in: 0..100, message: 'Max 100 characteres' }
+  validates :title, presence: true, length: { in: 0..100, message: 'Max 100 characteres' }
   validates :description, length: { in: 0..200, message: 'Max 200 characteres' }, allow_nil: true
   # validates :comments_count,  presence: true,
   #                             numericality: { only_integer: true, greater_than_or_equal: 0 }

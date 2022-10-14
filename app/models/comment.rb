2c5c95fb69ebf357186a9b9f4e6be88db8e25093
Jesus Barboza <jbarz20@gmail.com>
2022-10-13 23:21:13 -0500

    CHORE: modification max character 200

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    # belongs_to :commentable, polymorphic: true, counter_cache: :comments_count
-  validates :description, length: { in: 0..200, message: 'Max 200 characteres' }, allow_nil: true
+  belongs_to :commentable, polymorphic: true
+  validates :body, length: { in: 0..400, message: 'Max 400 characteres' }, allow_nil: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, presence: true, length: { in: 0..100, message: 'Max 100 characteres' }
-  validates :description, length: { in: 0..200, message: 'Max 200 characteres' }, allow_nil: true
+  validates :description, length: { in: 0..500, message: 'Max 500 characteres' }, allow_nil: true
   # validates :comments_count,  presence: true,
   #                             numericality: { only_integer: true, greater_than_or_equal: 0 }
 end

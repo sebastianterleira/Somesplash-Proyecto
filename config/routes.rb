7fa7ec6741b054041d9ae279205c55e4799b9349
Jesus Barboza <jbarz20@gmail.com>
2022-10-13 22:21:55 -0500

    ADD: routes for photo controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'photo/new'
+  get 'photo/edit'
+  get 'photo/show'
   # get 'category/index'
   # get 'category/new'
   # get 'category/edit'
   # get 'category/show'
   # get "/categories", to: "categories#index"
   resources :category
+  resources :photo
 end

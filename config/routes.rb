5ae72cfdf7c1fc0083511869bea3e509b269a65c
Jesus Barboza <jbarz20@gmail.com>
2022-10-15 14:33:57 -0500

    ADD: images.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :categories
   resources :photos
   resources :comments, only: %i[destroy new create]
-  get "/search", to: "photos#search"
+  get "/search-results", to: "photos#search-results"
 end
